c8ac822632ceca4240c00a582fc59f5bea0b53f3
mailsondias.dev <mailsondias.dev@gmail.com>
2026-02-02 10:32:08 -0300

    refactor Hero class constructor and Atacar method for improved readability

--- hero.js
+++ hero.js
@@ -1,9 +1,22 @@
 class Hero {
-    constructor(nome, idade, tipo){
-        this.nome = nome;
-        this.idade = idade;
-        this.tipo = tipo;
-    }
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
 
-    
+  Atacar() {
+    let ataque = "";
+
+    if (this.tipo === "mago") {
+      ataque = "usando magia";
+    } else if (this.tipo === "guerreiro") {
+      ataque = "usando espada";
+    } else if (this.tipo === "monge") {
+      ataque = "usando artes marciais";
+    } else {
+      ataque = "usando shuriken";
+    }
+    console.log(`O ${this.tipo} atacou ${ataque}`);
+  }
 }
